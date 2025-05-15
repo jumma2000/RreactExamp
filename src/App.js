d9fed93867b891be2dc62d45866d798a859cc5d3
jumma2000 <jumma2000@hotmail.com>
2025-05-15 19:03:16 +0200

    Fix line endings for src/App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         الهم  صلى وسلم على اشرف لاانبياء والمرسلين 
+        </p>
+        <p>
+          Edit jumma <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
